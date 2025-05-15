7757a609b6fd3855780068d821d4fdb1deb18846
Ukweh Chima Everest <ukweheverest@gmail.com>
2025-05-15 19:32:23 +0100

    Update landing-card.tsx

--- components/shared/landing-card.tsx
+++ components/shared/landing-card.tsx
@@ -14,7 +14,7 @@ function LandingCard() {
         setShowDrawer(show)
     }
     return (
-        <Box className={`flex ${(isBrowser && !isMobile) ? 'min-h-[500px]' : 'min-h-[350px]'} flex-col h-full items-start justify-center gap-10 px-4 md:flex-row md:items-center md:justify-between md:gap-0 md:px-32 pt-24 pb-4`}>
+        <Box className={`flex ${(isBrowser && !isMobile) ? 'min-h-[500px]' : 'min-h-[350px]'} flex-col h-full items-start justify-center gap-10 px-4 md:flex-row md:items-center md:justify-between md:gap-0 md:px-32 md:pt-24 pb-4`}>
             <Box className="z-10 md:my-auto flex min-h-[350px] w-full flex-col items-start justify-center gap-4 md:w-1/2">
                 <Box className="absolute bottom-6 right-4 h-[65px] w-[65px] max-md:opacity-0">
                     <Image
