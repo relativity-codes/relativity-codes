af1356624172b4293ff6418a6c70dfbbf6d17a85
Ukweh Chima Everest <ukweheverest@gmail.com>
2025-05-15 20:11:09 +0100

    Update landing-card.tsx

--- components/shared/landing-card.tsx
+++ components/shared/landing-card.tsx
@@ -61,8 +61,8 @@ function LandingCard() {
                     </Box>
                     <Box className="absolute -z-10 h-[500px] w-[500px] max-md:w-full max-md:aspect-square">
                         <Image 
-                            className="max-md:object-cover max-h-full max-w-full"
-                            fill
+{/*                             className="max-md:object-cover"
+                            fill */}
                             priority
                             src="/avatar.png" 
                             alt={''}
